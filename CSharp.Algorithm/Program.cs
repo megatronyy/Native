5fd1d6f2f549a3d27793d95024c2ea55b9aba229
屈文斌 <twfx7758@hotmail.com>
2016-05-18 22:59:44 +0800

    Sort

---
+++ CSharp.Algorithm/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[6] { 5, 3, 1, 4, 6, 2 };
+
+            Sort(arr);
+
+            Console.ReadKey();
+        }
+
+        static void Sort(int[] arr)
+        {
+            int len = arr.Length;
+            int i, j, t;
+            for (i = 0; i < len - 1; ++i)
+            {
+                for (j = 0; j < len - 1 - i; ++j)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        t = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = t;
+                    }
+                }
+            }
+
+            for (i = 0; i < len; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
