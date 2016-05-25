c244244c94c25e0593388603036065ecb3f20569
twfx7758@163.com <twfx7758@163.com>
2016-05-25 09:10:28 +0800

    SortBase

---
+++ CSharp.Algorithm/Sort/SortBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Algorithm.Sort
+{
+    public abstract class SortBase<T> where T : IComparable
+    {
+        public abstract void Sort(T[] a);
+
+        public bool Less(T v, T w)
+        {
+            return v.CompareTo(w) < 0;
+        }
+
+        public void Exch(T[] a, int i, int j)
+        {
+            T t = a[i];
+            a[i] = a[j];
+            a[j] = t;
+        }
+
+        public bool IsSort(T[] a)
+        {
+            for (int i = 1; i < a.Length; i++)
+            {
+                if(Less(a[i], a[i-1])) 
+                    return false;
+            }
+            return true;
+        }
+    }
+}
