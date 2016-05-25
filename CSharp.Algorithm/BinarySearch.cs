c149676de98e06dc9112781c9937b5b304da4e2f
twfx7758@163.com <twfx7758@163.com>
2016-05-25 13:44:46 +0800

    二分查找

---
+++ CSharp.Algorithm/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Algorithm
+{
+    public class BinarySearch
+    {
+        public static int Rank(int key, int[] a)
+        {
+            //数组必须是有序的
+            int low = 0;
+            int high = a.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+                if (a[mid] < key)
+                    high = mid - 1;
+                else if (a[mid] > key)
+                    low = mid + 1;
+                else
+                    return mid;
+            }
+
+            return -1;
+        }
+    }
+}
