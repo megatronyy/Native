3caac1153fc26da6176d17c0650f057148b856a3
twfx7758@163.com <twfx7758@163.com>
2016-06-08 10:26:53 +0800

    modify

---
+++ Algorithm/LongestPrefix.cpp
@@ -0,0 +1,12 @@
+#include "LongestPrefix.h"
+
+
+void LongestPrefix::TestMothed()
+{
+
+}
+
+string LongestPrefix::LongestCommonPrefix(vector<string> &strs)
+{
+	return "";
+}
---
+++ Algorithm/LongestPrefix.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+#include <vector>
+using namespace std;
+
+class LongestPrefix
+{
+public:
+	void TestMothed();
+
+private:
+	string LongestCommonPrefix(vector<string> &strs);
+};
+
